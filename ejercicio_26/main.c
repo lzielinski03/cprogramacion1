29dbb77346bb85a7ab893c257e630bb83422c598
lzielinski03 <lzielinski03@gmail.com>
2013-09-30 10:37:47 -0300

    Ejercicio 26
    Windows 7 profesional 64bits
    IDE Netbeans
    
    Leer 20 números enteros positivos. Mostrar el vector tal como fue
    ingresado y luego mostrar el vector ordenado en forma decreciente.
    
    Seleccion invertida.

--- ejercicio_26/main.c
+++ ejercicio_26/main.c
@@ -15,10 +15,7 @@
  */
 const int n = 20;
 int enteros[20], i, x, y, aux, menor, mayor;
-void burbujeo();
-void seleccion();
-void insercion();
-void burbujeoBidireccional();
+void seleccionInvertida();
 
 void main() {
     srand(time(NULL));
@@ -28,68 +25,22 @@ void main() {
         printf("Posicion: %d\tValor: %d\n", i, enteros[i]);
     }
     printf("----------\n");
-    burbujeoBidireccional();
+    seleccionInvertida();
     for (i = 0; i < n; i++) {
         printf("Posicion: %d\tValor: %d\n", i, enteros[i]);
     }
     system("pause");
 }
 
-void burbujeo() {
-    for (x = 0; x < n - 1; x++) {
-        for (y = 0; y < n - 1; y++) {
-            if (enteros[y] > enteros[y + 1]) {
-                aux = enteros[y + 1];
-                enteros[y + 1] = enteros[y];
-                enteros[y] = aux;
-            }
-        }
-    }
-}
-
-void burbujeoBidireccional(){
-    int ultimo, izquierda, derecha;
-    do{
-        for(x = n-1; x > 0; x--){
-            if(enteros[x-1] > enteros[x]){
-                aux = enteros[x];
-                enteros[x] = enteros[x-1];
-                enteros[x-1] = aux;
-                ultimo = x;
-            }
-        }
-        izquierda = ultimo+1;
-        for(x = 1; x < n-1; x++){
-            if(enteros[x-1] > enteros[x]){
-                aux = enteros[x];
-                enteros[x] = enteros[x-1];
-                enteros[x-1] = aux;
-                ultimo = x;
-            }
-        }
-        derecha = ultimo-1;   
-    }while(derecha >= izquierda);
-}
-
-void seleccion() {
+void seleccionInvertida() {
     for (x = 0; x < n; x++) {
         menor = x;
         for (y = x + 1; y < n; y++) {
-            if (enteros[menor] > enteros[y])
+            if (enteros[menor] < enteros[y])
                 menor = y;
         }
         aux = enteros[x];
         enteros[x] = enteros[menor];
         enteros[menor] = aux;
-    }
-}
-
-void insercion() {
-    for (x = 1; x < n; x++) {
-        aux = enteros[x];
-        for (y = x - 1; y >= 0 && aux < enteros[y]; y--) {
-            enteros[y + 1] = enteros[y];
-        }
-        enteros[y + 1] = aux;
     }
 }
